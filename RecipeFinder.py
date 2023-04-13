611f63450de93c1981227425c8fc587e8c6a3711
Oliver Clarke <olivercameronclarke@gmail.com>
2023-04-13 14:59:39 -0400

    web scraper updates

--- RecipeFinder.py
+++ RecipeFinder.py
@@ -14,14 +14,18 @@ def get_recipe(link):
     thumbnail_url = thumbnail['src']
     try:
             prep_time = soup.find('div', class_ = 'loc prep-time project-meta__prep-time').find('span', class_ = 'meta-text__data').text
+            prep_time = convert_to_minutes(prep_time)
     except (TypeError, AttributeError):
             prep_time = ""
     
     cook_time = soup.find('div', class_ = 'loc cook-time project-meta__cook-time').find('span', class_ = 'meta-text__data').text
+    cook_time = convert_to_minutes(cook_time)
     total_time = soup.find('div', class_ = 'loc total-time project-meta__total-time').find('span', class_ = 'meta-text__data').text
+    total_time = convert_to_minutes(total_time)
     servings = soup.find('div', class_ = 'loc recipe-serving project-meta__recipe-serving').find('span', class_ = 'meta-text__data').text.replace('\n',' ')
     
-    ingredient_list = []
+    
+    ingredient_list = ""
 
     raw_ingredients = soup.find_all('li', class_ = 'structured-ingredients__list-item')
     for ingredient in raw_ingredients:
@@ -30,10 +34,11 @@ def get_recipe(link):
         for span in spans:
             if span.text not in ingredient_entry:
                 ingredient_entry += span.text + " "
-        ingredient_list.append(ingredient_entry)
+        ingredient_list += ingredient_entry + ","
+    ingredient_list = ingredient_list[:-1]
 
-    instruction_list = []
-    image_urls_list = []
+    instruction_list = ""
+    image_urls_list = ""
 
     raw_instructions = soup.find_all('li', class_ = 'comp mntl-sc-block-group--LI mntl-sc-block mntl-sc-block-startgroup') 
     for instruction in raw_instructions:
@@ -50,30 +55,34 @@ def get_recipe(link):
             instruction_images = ""
             image_url = "no-image"
         
+        image_urls_entry = " "
         if (instruction_images != ""):
-            image_urls_entry = []
             for instruction_image in instruction_images:
                 image = instruction_image.find('img', class_ = 'universal-image__image')
                 image_url = image['data-src']
-                image_urls_entry.append(image_url)
+                image_urls_entry += image_url + ","
+            image_urls_entry = image_urls_entry[:-1]
 
-        instruction_entry = []
-        true_instruction_text = '"' + instruction_text + '"'
-        instruction_entry.append(instruction_name)
-        instruction_entry.append(true_instruction_text)
-        image_urls_list.append(image_urls_entry)
-        instruction_list.append(instruction_entry)
+        instruction_entry = ""
+        true_instruction_text = instruction_text.replace(",", ";")
+        instruction_entry += instruction_name + ","
+        instruction_entry += true_instruction_text
+        instruction_list += instruction_entry + ","
 
+        image_urls_list += image_urls_entry + ","
+        
+    instruction_list = instruction_list[1:-1]
+    image_urls_list = image_urls_list[:-1]
 
-    recipe_dict = { 'recipe_name' : recipe_name,
-                'thumbnail_url' : thumbnail_url,
-                'cook_time' : cook_time, 
-                'prep_time' : prep_time,
-                'total_time' : total_time,
+    recipe_dict = { 'name' : recipe_name,
+                'thumbnail' : thumbnail_url,
+                'cooktime' : cook_time, 
+                'preptime' : prep_time,
+                'totaltime' : total_time,
                 'servings' : servings,
                 'ingredients' : ingredient_list,  
                 'instructions' : instruction_list,
-                'image_urls' : image_urls_list
+                'images' : image_urls_list
                 #'rating' : rating,
                 }
 
@@ -81,9 +90,9 @@ def get_recipe(link):
 
 def write_to_csv(dictionary): # should be a list of dictionaries
     csv_file = "recipes.csv"
-    field_names = ['recipe_name', 'thumbnail_url', 'cook_time', 'prep_time', 'total_time', 'servings', 'ingredients', 'instructions', 'image_urls']
+    field_names = ['name', 'thumbnail', 'cooktime', 'preptime', 'totaltime', 'servings', 'ingredients', 'instructions', 'images']
     try:
-        with open(csv_file, 'w', encoding="utf-8") as csvfile:
+        with open(csv_file, 'w', newline='', encoding="utf-8") as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=field_names)
             writer.writeheader()      
             writer.writerows(dictionary)     
@@ -106,6 +115,16 @@ def get_urls(parent_url):
     res = res[ : -1]
 
     return(res)
+
+def convert_to_minutes(time_string):
+    parts = time_string.split()
+    minutes = 0
+    for i in range(len(parts)):
+        if parts[i] == "hrs":
+            minutes += int(parts[i-1]) * 60
+        elif parts[i] == "mins":
+            minutes += int(parts[i-1])
+    return minutes
 
 def main():
     parent_url = 'https://www.simplyrecipes.com/vegan-chicken-nuggets-recipe-7091022'
--- app/(tabs)/lets-cook.tsx
+++ app/(tabs)/lets-cook.tsx
@@ -1,29 +1,46 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, SafeAreaView, ScrollView } from 'react-native';
 import { Text, View } from '../../components/Themed';
+import { Popular } from '../../components'
+import { COLORS, icons, images, SIZES } from '../../constants';
 
 export default function TabThreeScreen() {
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Lets Cook!</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.welcome}>
+          <Text style={styles.title}>Lets Cook!</Text>
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+          <Popular/>
+        </View>
+      </ScrollView>
+  </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: COLORS.lightWhite,
+  },
+  welcome: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: COLORS.lightWhite,
+    marginRight: 20,
+    marginLeft: 20,
+    marginVertical: 30,
   },
   title: {
     fontSize: 20,
+    paddingTop: 20,
     fontWeight: 'bold',
+    color: COLORS.gray
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
-  },
+    width: '80%'
+  }
 });
